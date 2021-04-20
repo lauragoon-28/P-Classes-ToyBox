3500e78caf6593eceae27267de7139c1359ccaee
Laura Goon <Laura.goon@ou.edu>
2021-04-20 18:00:24 -0500

    created a solution and started the classes for this participation

---
+++ P_Classes_ToyBox/P_Classes_ToyBox/ToyBox.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P_Classes_ToyBox
+{
+    class ToyBox
+    {
+        public List<Toy> Toys { get; set; }
+
+        public ToyBox()
+        {
+            Toys = new List<Toy>();
+
+        }
+
+
+
+
+
+
+    }
+}
